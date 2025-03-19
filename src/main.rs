4bb53463013177840e4bb24053ea6998f8a4489a
mateolafalce <mateolafalce3@gmail.com>
2025-03-18 21:47:27 -0300

    0.2.0

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,13 @@
-use axum::response::IntoResponse;
 use axum::{
     Router,
     extract::ws::{Message, WebSocket, WebSocketUpgrade},
-    response::Html,
+    response::{Html, IntoResponse},
     routing::get,
 };
+use clap::Parser;
 use futures_util::{SinkExt, StreamExt};
 use std::{
-    fs,
+    fs,process::Command,
     net::SocketAddr,
     sync::{
         Arc,
@@ -16,18 +16,56 @@ use std::{
 };
 use tokio::sync::{Mutex, broadcast};
 
-// TODO impl this as a CLI args
-// TODO impl title as a CLI
-// TODO just 1 person should be able to share the screen
-// TODO add counter of active/waiting users
-// TODO add waiting image for client
+/// Default port
 const PORT: &str = "3000";
+/// Default title
+const TITLE: &str = "shared";
+
+#[derive(Clone)]
+struct AppState {
+    state: Arc<Mutex<broadcast::Sender<Vec<u8>>>>,
+    is_sharing: Arc<AtomicBool>,
+    title: String,
+}
+
+impl AppState {
+    fn new(title: String) -> Self {
+        let (tx, _) = broadcast::channel::<Vec<u8>>(50);
+        let state = Arc::new(Mutex::new(tx));
+        let is_sharing = Arc::new(AtomicBool::new(false));
+        Self {
+            state,
+            is_sharing,
+            title,
+        }
+    }
+}
+
+#[derive(Parser, Debug)]
+#[command(version, about, long_about = None)]
+struct Args {
+    /// The maximum number of messages you want to store
+    #[arg(short, long)]
+    port: Option<String>,
+    /// The cost of the Proof of Work that will be sent to the client
+    #[arg(short, long)]
+    title: Option<String>,
+}
 
 #[tokio::main]
 async fn main() {
-    let (tx, _) = broadcast::channel::<Vec<u8>>(50);
-    let tx = Arc::new(Mutex::new(tx));
-    let is_sharing = Arc::new(AtomicBool::new(false));
+    let args = Args::parse();
+
+    let port: String = match args.port {
+        Some(port) => port,
+        None => PORT.to_string(),
+    };
+
+    let title: String = match args.title {
+        Some(title) => title,
+        None => TITLE.to_string(),
+    };
+    let state = AppState::new(title);
 
     let app = Router::new()
         .route("/admin", get(serve_index))
@@ -37,15 +75,31 @@ async fn main() {
             "/static",
             axum::routing::get_service(tower_http::services::ServeDir::new("./static")),
         )
-        .with_state((tx, is_sharing));
+        .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{PORT}"))
+    let listener = tokio::net::TcpListener::bind(format!("0.0.0.0:{port}"))
         .await
         .unwrap();
 
-    println!("In localhost http://127.0.0.1:{PORT}");
-    // TODO get 192.168.1.47 in a dynamic way in linux, Win and Mac
-    println!("For local network http://192.168.1.47:{PORT}");
+    println!("In localhost http://127.0.0.1:{port}");
+    // TODO get local network ip in Win and Mac
+    if cfg!(target_os = "linux") {
+        let output = Command::new("sh")
+            .arg("-c")
+            .arg("hostname -I | awk '{print $1}'")
+            .output()
+            .expect("Failed to execute command");
+
+        if output.status.success() {
+            let ip = String::from_utf8_lossy(&output.stdout);
+            println!("For local network http://{}:{port}", ip.trim());
+        } else {
+            eprintln!(
+                "Command failed: {}",
+                String::from_utf8_lossy(&output.stderr)
+            );
+        }
+    }
 
     axum::serve(
         listener,
@@ -57,9 +111,12 @@ async fn main() {
 
 macro_rules! serve_html {
     ($func_name:ident, $file_path:expr) => {
-        async fn $func_name() -> Html<String> {
-            let html = fs::read_to_string($file_path)
-                .unwrap_or_else(|_| "<h1>Error</h1>".to_string());
+        async fn $func_name(
+            axum::extract::State(state): axum::extract::State<AppState>,
+        ) -> Html<String> {
+            let mut html =
+                fs::read_to_string($file_path).unwrap_or_else(|_| "<h1>Error</h1>".to_string());
+            html.push_str(&format!("<title>{}</title>", state.title));
             Html(html)
         }
     };
@@ -70,12 +127,15 @@ serve_html!(client_index, "static/client.html");
 
 async fn ws_handler(
     ws: WebSocketUpgrade,
-    axum::extract::State((state, is_sharing)): axum::extract::State<(
-        Arc<Mutex<broadcast::Sender<Vec<u8>>>>,
-        Arc<AtomicBool>,
-    )>,
+    axum::extract::State(state): axum::extract::State<AppState>,
 ) -> impl IntoResponse {
-    ws.on_upgrade(move |socket| handle_socket(socket, state, is_sharing))
+    ws.on_upgrade(move |socket| {
+        handle_socket(
+            socket,
+            state.state,
+            state.is_sharing,
+        )
+    })
 }
 
 async fn handle_socket(
@@ -87,6 +147,13 @@ async fn handle_socket(
     let mut rx = state.lock().await.subscribe();
     let state_clone = state.clone();
     let is_sharing_clone = is_sharing.clone();
+
+    if is_sharing_clone.load(Ordering::SeqCst) {
+        let _ = ws_sender.send(Message::Text(r#"{"type":"sharing_status","status":"busy"}"#.into())).await;
+    } else {
+        let _ = ws_sender.send(Message::Text(r#"{"type":"sharing_status","status":"available"}"#.into())).await;
+    }
+
     tokio::spawn(async move {
         while let Some(Ok(msg)) = ws_receiver.next().await {
             match msg {
